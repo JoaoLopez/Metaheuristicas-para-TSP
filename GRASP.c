c45fa72ce2c953c8dd3ac6172a5deb0e85a629ee
JoaoLopez <joaolopez@id.uff.br>
2022-07-15 10:39:00 -0300

    GRASP.c testado e funcionando

--- GRASP.c
+++ GRASP.c
@@ -12,12 +12,24 @@ int executarGRASP(InstanciaTSP* instanciaTSP, int numeroRepeticoes, double alpha
         melhorSolucao = getMelhorSolucaoInstanciaTSP(instanciaTSP);
         melhorCusto = getCustoMelhorSolucaoInstanciaTSP(instanciaTSP);
 
-        apagarSolucaoInstanciaTSP(instanciaTSP);
+        removerSolucaoInstanciaTSP(instanciaTSP);
     }
+
+
+printf("Custo Solução inicial: %lf\n", melhorCusto);
+printf("Solução Inicial:\n");
+imprimirTour(melhorSolucao);
 
 
     int statusOperacao = -1;
     for(int i = 0; i < numeroRepeticoes; i++){
+
+
+
+printf("------------------------------- Nova Iteração -------------------------------\n");
+
+
+
         statusOperacao = solucionarHeuristicaVizinhoMaisProximoDoisLadosRandomizada(instanciaTSP, alpha);
         if(statusOperacao != OK){
             deletarTour(melhorSolucao);
@@ -28,9 +40,13 @@ int executarGRASP(InstanciaTSP* instanciaTSP, int numeroRepeticoes, double alpha
         if(statusOperacao != OK){
             deletarTour(melhorSolucao);
             deletarTour(getMelhorSolucaoInstanciaTSP(instanciaTSP));
-            apagarSolucaoInstanciaTSP(instanciaTSP);
+            removerSolucaoInstanciaTSP(instanciaTSP);
             return ERRO_EXECUTAR_BUSCA_LOCAL_INSTANCIA_TSP;
         }
+
+
+printf("Custo Nova Solução: %lf\n", getCustoMelhorSolucaoInstanciaTSP(instanciaTSP));
+
 
         if(getCustoMelhorSolucaoInstanciaTSP(instanciaTSP) < melhorCusto ||
         melhorCusto == -1){
@@ -40,6 +56,15 @@ int executarGRASP(InstanciaTSP* instanciaTSP, int numeroRepeticoes, double alpha
 
             melhorCusto = getCustoMelhorSolucaoInstanciaTSP(instanciaTSP);
             melhorSolucao = getMelhorSolucaoInstanciaTSP(instanciaTSP);
+
+
+printf("\nMelhor Solução Atualizada!\n");
+printf("Nova Solução:\n");
+imprimirTour(melhorSolucao);
+printf("\n");
+
+
+
         }
         else{
             deletarTour(getMelhorSolucaoInstanciaTSP(instanciaTSP));
@@ -47,9 +72,15 @@ int executarGRASP(InstanciaTSP* instanciaTSP, int numeroRepeticoes, double alpha
 
         //Resetando solução de instância TSP
         //Necessário pois a heurística construtiva só sobrescreverá a solução
-        //de instanciaTSP se encntrar uma solução melhor.
-        apagarSolucaoInstanciaTSP(instanciaTSP);
+        //de instanciaTSP se encontrar uma solução melhor.
+        removerSolucaoInstanciaTSP(instanciaTSP);
     }
+
+printf("------------------------------- FIM -------------------------------\n");
+printf("Custo da Melhor Solução Encontrada: %lf\n", melhorCusto);
+printf("Melhor Solução Encontrada:\n");
+imprimirTour(melhorSolucao);
+
 
     setMelhorSolucaoInstanciaTSP(instanciaTSP, melhorSolucao);
     return OK;
