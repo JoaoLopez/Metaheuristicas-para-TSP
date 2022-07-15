c7473a6a92f4cd63777e6f4cf4004812ff1e0304
JoaoLopez <joaolopez@id.uff.br>
2022-07-15 16:27:04 -0300

    Perturbação da solução testada e funcionando corretamente

--- ILS.c
+++ ILS.c
@@ -4,11 +4,11 @@ int executarILS(InstanciaTSP* instanciaTSP, int numeroRepeticoes, double alpha){
     NoTour *melhorSolucao = NULL;
     double melhorCusto = -1;
 
-
+/*
 printf("Grafo\n");
 imprimirGrafo(instanciaTSP->grafo);
 printf("Grafo\n");
-
+*/
     //Obtendo uma solução inicial
     int statusOperacao = -1;
     if(getMelhorSolucaoInstanciaTSP(instanciaTSP) == NULL){
@@ -29,11 +29,12 @@ printf("Grafo\n");
         removerSolucaoInstanciaTSP(instanciaTSP);
         return ERRO_COPIAR_TOUR;
     }
-    
+
+/*    
 printf("Custo Solução Inicial: %lf\n", melhorCusto);
 printf("Solução Inicial:\n");
 imprimirTour(melhorSolucao);
-
+*/
 
     //solucao1 e custo1 armazenam a solução que o ILS está utilizando como base
     NoTour *solucao1 = melhorSolucao, *solucoes_antigas[5], *aux;
@@ -42,13 +43,15 @@ imprimirTour(melhorSolucao);
     for(int i = 0; i < numeroRepeticoes; i++){
         perturbarSolucao(instanciaTSP);
 
+/*
 printf("-------------------------- NOVA ITERAÇÃO --------------------------\n");
 printf("Custo da Pertubação: %lf\n", getCustoMelhorSolucaoInstanciaTSP(instanciaTSP));
-
+*/
         statusOperacao = executarBuscaLocalMelhorAprimorante(instanciaTSP);
 
+/*
 printf("Custo da Nova Solução: %lf\n", getCustoMelhorSolucaoInstanciaTSP(instanciaTSP));
-
+*/
 
         if(statusOperacao != OK){
             deletarTour(melhorSolucao);
@@ -62,9 +65,10 @@ printf("Custo da Nova Solução: %lf\n", getCustoMelhorSolucaoInstanciaTSP(insta
 
         //Se a nova solução for a melhor obtida até agora será a utilizada na próxima perturbação
         if(getCustoMelhorSolucaoInstanciaTSP(instanciaTSP) < melhorCusto){
-
+/*
 printf("\nSolução Melhor Encontrada!\n");
 imprimirTour(instanciaTSP->melhorSolucao);
+*/
 
             deletarTour(melhorSolucao);
             if(solucao1 != melhorSolucao)
@@ -80,9 +84,9 @@ imprimirTour(instanciaTSP->melhorSolucao);
 
 
 
-
+/*
 printf("\nCusto da Próxima Solução Perturbada: %lf\n", custo1);
-
+*/
 
 
 
@@ -91,7 +95,9 @@ printf("\nCusto da Próxima Solução Perturbada: %lf\n", custo1);
         //então ela é armazenada no histórico. A solução perturbada na próxima iteração
         //será a mesma que foi perturbada nessa iteração
         else if(repeticoes_sem_melhora < 5){
+/*
 printf("Solução Adicionada ao Histórico\n");
+*/
 
             solucoes_antigas[repeticoes_sem_melhora] = getMelhorSolucaoInstanciaTSP(instanciaTSP);
             custos_antigos[repeticoes_sem_melhora] = getCustoMelhorSolucaoInstanciaTSP(instanciaTSP);
@@ -100,7 +106,9 @@ printf("Solução Adicionada ao Histórico\n");
         //Se a nova solução não foi a melhor de todas e não há mais espaço no histórico,
         //então a melhor solução do histórico será escolhida para ser perturbada
         else{
+/*
 printf("Atualização com Piora da Solução Base\n");
+*/
 
             double temp = getCustoMelhorSolucaoInstanciaTSP(instanciaTSP);
             NoTour* temp2 = getMelhorSolucaoInstanciaTSP(instanciaTSP);
@@ -121,9 +129,9 @@ printf("Atualização com Piora da Solução Base\n");
             custo1 = temp;
             solucao1 = temp2;
 
-
+/*
 printf("\nCusto da Próxima Solução Perturbada: %lf\n", custo1);
-
+*/
 
 
         }
@@ -151,70 +159,63 @@ printf("\nCusto da Próxima Solução Perturbada: %lf\n", custo1);
 }
 
 void perturbarSolucao(InstanciaTSP* instanciaTSP){
-    return;
+    int numCidades = getDimensaoInstanciaTSP(instanciaTSP);
+    double porcentModificacao = sortearNumeroAleatorio(10, 30)/100.0;
+    int numModificacoes = (int) (numCidades*porcentModificacao);
+    int arestasSorteadas[numModificacoes];
+    NoTour* subtoursGerados[numModificacoes+1];
+    sortearNumerosAleatoriosDistintos(numModificacoes, 1, numCidades, arestasSorteadas);
+    ordenar_vetor(arestasSorteadas, numModificacoes);
 
 
-    
-    double porcent_modificacao = sortearNumeroAleatorio(10, 60)/100.0;
-    int num_modificacoes = (int) (instanciaTSP->dimensao*porcent_modificacao);
-    int arestas_sorteadas[num_modificacoes];
-    NoTour* subtours_gerados[num_modificacoes+1];
-    sortearNumerosAleatoriosDistintos(num_modificacoes, 1, instanciaTSP->dimensao, arestas_sorteadas);
-    ordenar_vetor(arestas_sorteadas, num_modificacoes);
-    deletarArestasTour(instanciaTSP->melhorSolucao, arestas_sorteadas, num_modificacoes, subtours_gerados);
-    int subtour_i, subtour_j, lim_sup = num_modificacoes;
+imprimirTour(instanciaTSP->melhorSolucao);
+
+    deletarArestasTour(getMelhorSolucaoInstanciaTSP(instanciaTSP), arestasSorteadas, numModificacoes, subtoursGerados);
+    int subtour_i, subtour_j, lim_sup = numModificacoes;
     NoTour* aux;
     
-    /*
-    printf("porcent: %lf\n", porcent_modificacao);
-    printf("num_modificacoes: %d\n", num_modificacoes);
-    for(int i=0; i < num_modificacoes+1; i++){
-        printf("Imprimindo subtour_gerado[%d]:\n", i);
-        imprimirTour(subtours_gerados[i]);
-        printf("\n\n");
-    }
-    */
+    
+printf("porcent: %lf\n", porcentModificacao);
+printf("numModificacoes: %d\n", numModificacoes);
+for(int i=0; i < numModificacoes+1; i++){
+    printf("Imprimindo subtour_gerado[%d]:\n", i);
+    imprimirTour(subtoursGerados[i]);
+    printf("\n\n");
+}
+
     while(lim_sup > 2){
         //O primeiro e o último subtours precisam ser unidos sempre à esquerda e à direita
         //respectivamente. Por isso são excluidos das uniões aleatórias
         subtour_i = lim_sup-1;
         subtour_j = sortearNumeroAleatorio(1, lim_sup-2);
         if(sortearNumeroAleatorio(0, 1)){
-            aux = unirSubTours(subtours_gerados[subtour_i], subtours_gerados[subtour_j]);
+            aux = unirSubTours(subtoursGerados[subtour_i], subtoursGerados[subtour_j]);
+
+printf("i -> j: ");
+
         }
         else{
-            aux = unirSubTours(subtours_gerados[subtour_j], subtours_gerados[subtour_i]);
+            aux = unirSubTours(subtoursGerados[subtour_j], subtoursGerados[subtour_i]);
+
+printf("j -> i: ");
+
+
         }
-        subtours_gerados[subtour_j] = aux;
+        subtoursGerados[subtour_j] = aux;
         lim_sup -= 1;
         
-        /*
-        printf("i: %d\n", subtour_i);
-        printf("j: %d\n\n", subtour_j);
+printf("i: %d   j: %d\n", subtour_i, subtour_j);
 
-        for(int i=0; i < lim_sup+1; i++){
-            printf("Imprimindo subtour_gerado[%d]:\n", i);
-            imprimirTour(subtours_gerados[i]);
-            printf("\n\n");
-        }
-        */
     }
-    /*
-    imprimirTour(subtours_gerados[0]);
-    printf("\n\n");
-    imprimirTour(subtours_gerados[1]);
-    printf("\n\n");
-    imprimirTour(subtours_gerados[num_modificacoes]);
-    */
 
-    aux = unirSubTours(subtours_gerados[0], subtours_gerados[1]);
-    if(num_modificacoes > 1)
-        aux = unirSubTours(aux, subtours_gerados[num_modificacoes]);
+    aux = unirSubTours(subtoursGerados[0], subtoursGerados[1]);
+    if(numModificacoes > 1)
+        aux = unirSubTours(aux, subtoursGerados[numModificacoes]);
 
-    /*
-    imprimirTour(aux);
-    printf("\n\n");
-    imprimirTour(instanciaTSP->melhorSolucao);
-    */
+    removerSolucaoInstanciaTSP(instanciaTSP);
+    setMelhorSolucaoInstanciaTSP(instanciaTSP, aux);
+
+imprimirTour(instanciaTSP->melhorSolucao);
+
     return;
 }
